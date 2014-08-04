06cc31088dcf6700f683585db7a9bedd37364682
Dan Seminara <dseminara@50onred.com>
2014-08-04 11:19:20 -0400

    Add order matching

--- orderCollector.py
+++ orderCollector.py
@@ -11,28 +11,29 @@ Options:
 
 '''
 
-import re, json
+import re, json, redis
 from docopt import docopt
 from schema import Use, Schema
 from flask import Flask, request
 
 app = Flask(__name__)
 
-prefix = ''
+prefix = 'orderbot'
 
-def payload(text): return {"channel": "#seamless-thursday", "username": "SeamlessBot", "text": text, "icon_emoji": ":seamless:"}
+def payload(text): return {"channel": "#seamless-thursday", "username": "OrderBot", "text": text, "icon_emoji": ":seamless:"}
+
+db = redis.StrictRedis()
 
 @app.route('/', methods=['POST'])
 def save_order():
-    post = request.form['text']
-    user = request.form['user_name']
-    # Find restaurant name
-    # Save order (only if prefix matches)
-    # reply with confirmation (with username in it)
-    # or with error message if restaurant name can't be found
-    # Or have some contextual ignore
-
-    return post_message("")
+    post  = request.form['text'].lower().strip()
+    user  = request.form['user_name']
+    order = re.match(r'%s\s*?:(.+?):(.+)' % prefix, post)
+    if order:
+        order.group(1).strip() # restaurant
+        order.group(2).strip() # entree
+        return post_message("")
+    return ""
 
 def post_message(message):
     return json.dumps(payload(message))
