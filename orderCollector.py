e01a25d48704e725259cb873322d75f08aa6302f
timmil <tmiller@50onred.com>
2014-08-04 17:02:28 +0000

    added better things

--- orderCollector.py
+++ orderCollector.py
@@ -22,7 +22,6 @@ exptime = 60*60*24*2 # Two days
 db      = redis.StrictRedis()
 special_user_orders = {}   #orders which don't match regular restaurants and are pending classification
 
-
 with open('restaurantList.txt') as f:
     restaurants = json.load(f)
 
@@ -30,26 +29,43 @@ restaurants = [[r.lower() for r in rest] for rest in restaurants] # Convert to l
 
 def payload(text): return {"channel": "#seamless-thursday", "username": "OrderBot", "text": text, "icon_emoji": ":seamless:"}
 
+def add_order(user, restaurant, entree):
+    key = 'orders:%s' % restaurant
+    db.rpush(key, '%s: %s' % (user, entree))
+    db.expire(key, exptime)
+
 @app.route('/', methods=['POST'])
 def save_order():
     post  = request.form['text'].lower().strip()
     user  = request.form['user_name']
     order = re.match(r'%s\s*?:(.+?):(.+)' % prefix, post)
-
-    if re.match(r'%s[,.:\- ;]help' % prefix, post):
-        return post_message('Order with this format: ```orderBot: restaurant: order```.  For example, ```orderBot: Mizu: Lunch Special, Spicy Tuna Roll, Yellowtail Roll, Salmon Roll, special instructions "Label Jim, extra spicy"'
-
+    
+    if user in special_user_orders:
+        if post = "yes" or post = "y":
+            add_order(user, *special_user_orders[user])
+            temp = special_user_orders[user][0]
+            del special_user_orders[user]
+            return "Great!  I'll add your order to %s under the category of miscellaneous restuarants" % temp
+        elif post = "no" or post = "n":
+            del special_user_orders[user]
+            return post_message: "Okay, I won't add your order. Feel free to place a new one."
+        else:
+            return post_message("""I'm sorry %s, I don't understand.  Do you want to add your order of ```%s``` 
+                                to the miscellaneous restaurant ```%s```?  Please answer yes or no.""" % (user, special_user_orders[user][1], special_user_orders[user][0]))
+        
+    elif re.match(r'%s[,.:\- ;]help' % prefix, post):
+        return post_message('''Order with this format: ```orderBot: restaurant: order```.  For example: 
+                            ```orderBot: Mizu: Lunch Special, Spicy Tuna Roll, Yellowtail Roll, Salmon Roll, special instructions "Label Jim, extra spicy"''')
     elif order:
-        restaurant = order.group(1).strip()
-        entree     = order.group(2).strip()
-        for r in restaurants:
-            if restaurant in r:
-                db.rpush('orders:%s' % r[0], '%s: %s' % (user, entree))
-                db.expire('orders:%s' % r[0], exptime)
-                return post_message("%s your order to %s was added successfully" % (user, r[0]))
+        r = order.group(1).strip() # restaurant
+        e = order.group(2).strip() # entree
+        for restaurant in restaurants:
+            if r in restaurant:
+                add_order(user, restaurant[0], e)
+                return post_message("%s your order to %s was added successfully" % (user, restaurant[0]))
 
-        special_user_orders[user] = (restaurant, entree)
-        return post_message("%s, %s is not one of our usual restaurants.  Should we save your order in the \"Miscellaneous Restaurant\" list? Yes/No" % (user, restaurant))
+        special_user_orders[user] = (r, e)
+        return post_message('%s, %s is not one of our usual restaurants.  Should we save your order in the "Miscellaneous Restaurant" list? Yes/No' % (user, r))
     return ""
 
 def post_message(message):
